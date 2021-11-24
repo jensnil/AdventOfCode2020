c2ca4c46f798cebdbcc980c95ea46ac0f38d7928
Jens Nilsson <jens.nilsson@leovegas.com>
2021-11-24 22:40:46 +0100

    Day 4_1

---
+++ src/bin/day4_1.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    let filename = "input/day04.txt";
+    // --snip--
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    let passports  = contents.split("\n\n").collect::<Vec<_>>();
+
+    let hash_mapped_passports = passports.into_iter().map(| passport | {
+        passport.split(|c| { c == ' ' || c == '\n'}).map(|item| {
+            let tuple = item.split(":").collect::<Vec<_>>();
+            (tuple[0], tuple[1])
+        }).collect::<HashMap<_,_>>()
+    }).collect::<Vec<HashMap<_, _>>>();
+
+    let valid_ones = hash_mapped_passports.into_iter().filter(|passport| {
+        passport.contains_key("byr") &&
+            passport.contains_key("iyr") &&
+            passport.contains_key("eyr") &&
+            passport.contains_key("hgt") &&
+            passport.contains_key("hcl") &&
+            passport.contains_key("ecl") &&
+            passport.contains_key("pid")
+    }).collect::<Vec<HashMap<_,_>>>();
+    println!("{}", valid_ones.len())
+}
